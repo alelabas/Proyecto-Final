fad3c240c395504961a479a4aa4e44d2c33ee5af
Mati4321 <amatiasalviso@gmail.com>
2024-11-04 19:15:40 -0300

    Revert "pulida para los turnos, confirmaciones y demas"
    This reverts commit 345d4ba098bd203db3f0e33d88e3dec244559314.

--- cancelar_turno.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../php/conexion.php';
-session_start();
-
-$id = $_GET['id'];
-
-mysqli_query($conexion, "UPDATE turno SET ESTADO_TURNO = 'CANCELADO' WHERE CODIGO_TURNO = '$id'");
-header("Location: ../html/vista_turnos_asignados.php");
--- conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost", "root", "", "proyecto_final") or die("No se pudo conectar a la base de datos");
-?>
