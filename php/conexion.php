9dc63e1c03191a89d85e7f8e0723b24bba31d6ec
Mati4321 <161222178+Mati4321@users.noreply.github.com>
2024-11-11 00:05:58 -0300

    Update conexion.php (sql "Proyecto_Final")

--- php/conexion.php
+++ php/conexion.php
@@ -1,3 +1,3 @@
 <?php
-    $conexion = mysqli_connect("localhost", "root", "", "proyecto_final_main") or die("No se pudo conectar a la base de datos");
+    $conexion = mysqli_connect("localhost", "root", "", "proyecto_final") or die("No se pudo conectar a la base de datos");
 ?>
