473996a51b587799ae0ffe3919a5cd587a4aa09d
alelabas <alelabas208@gmail.com>
2024-11-11 23:04:42 -0300

    Update enviar_contacto.php

--- php/enviar_contacto.php
+++ php/enviar_contacto.php
@@ -4,9 +4,24 @@
     $titulo = $_POST['titulo'];
     $mensaje = $_POST['mensaje'];
 
+    $destino = "soporte@servinow.com";
+    $asunto = "Contacto desde el sitio" ;
+    $mensaje = "Mail usuario: " . $correo . "Titulo: " . $titulo . "Mensaje: " . $mensaje; 
+    $header = "Email: " . $correo;
+
+    $enviado = @mail($destino, $asunto, $mensaje, $header);
+
+    if ($enviado == true) {
+        echo "Su mensaje ha sido enviado.";
+    }
+    else { echo "Su mensaje no pudo ser enviado";}
+
     include("conexion.php");
 
     $consulta = mysqli_query($conexion, "INSERT INTO MENSAJE (TITULO, MENSAJE, CORREO_ELECTRONICO) VALUES ('$titulo', '$mensaje', '$correo')");
+
+
+
     ?>
     <script>
     alert('El mensaje ha sido enviado correctamente');
